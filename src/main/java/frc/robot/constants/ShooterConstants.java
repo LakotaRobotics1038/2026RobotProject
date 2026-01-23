5662defa1ad3fa4f4fdc952b5e6867a43f256114
murat <99989538+Murat65536@users.noreply.github.com>
2026-01-22 20:33:29 -0500

    Start on shooter

---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public final class ShooterConstants {
+    private ShooterConstants() {
+    }
+
+    public static final int FIRST_TOP_SHOOTER_CAN_ID = 0;
+    public static final int FIRST_BOTTOM_SHOOTER_CAN_ID = 0;
+    public static final int SECOND_TOP_SHOOTER_CAN_ID = 0;
+    public static final int SECOND_BOTTOM_SHOOTER_CAN_ID = 0;
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ShooterConstants;
+
+public class Shooter extends SubsystemBase {
+    private final SparkMax firstTopShooter = new SparkMax(ShooterConstants.FIRST_TOP_SHOOTER_CAN_ID,
+            MotorType.kBrushless);
+    private final SparkMax firstBottomShooter = new SparkMax(ShooterConstants.FIRST_BOTTOM_SHOOTER_CAN_ID,
+            MotorType.kBrushless);
+    private final SparkMax secondTopShooter = new SparkMax(ShooterConstants.SECOND_TOP_SHOOTER_CAN_ID,
+            MotorType.kBrushless);
+    private final SparkMax secondBottomShooter = new SparkMax(ShooterConstants.SECOND_BOTTOM_SHOOTER_CAN_ID,
+            MotorType.kBrushless);
+
+    private static Shooter instance;
+
+    private Shooter() {
+        firstTopShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+        firstBottomShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+        secondTopShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+        secondBottomShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+    }
+
+    public static Shooter getInstance() {
+        if (instance == null) {
+            instance = new Shooter();
+        }
+        return instance;
+    }
+}
