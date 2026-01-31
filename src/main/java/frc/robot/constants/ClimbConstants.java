c4b7eba65c4dc35acd57e6ce1933bf2bfebaf392
Nathan <nathanyunoolee@gmail.com>
2026-01-31 14:04:32 -0500

    Climb Subsystem Edits

--- src/main/java/frc/robot/constants/ClimbConstants.java
+++ src/main/java/frc/robot/constants/ClimbConstants.java
@@ -5,5 +5,7 @@ public final class ClimbConstants {
     }
 
     public static final int CLIMB_MOTOR_CAN_ID = 0;
-    public static final double CLIMB_MOTOR_SPEED = 0;
+    public static final double CLIMB_MOTOR_SPEED_UP = 0;
+    public static final double CLIMB_MOTOR_SPEED_DOWN = 0;
+    public static final double MAX = 0;
 }
--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
@@ -12,8 +14,11 @@ import frc.robot.constants.ClimbConstants;
 public class Climb extends SubsystemBase {
 
     private final SparkMax climbMotor = new SparkMax(ClimbConstants.CLIMB_MOTOR_CAN_ID, MotorType.kBrushless);
-
+    private AbsoluteEncoder climbEncoder = climbMotor.getAbsoluteEncoder();
+    private SparkLimitSwitch limitSwitchBottom = climbMotor.getReverseLimitSwitch();
+    private SparkLimitSwitch limitSwitchTop = climbMotor.getForwardLimitSwitch();
     private static Climb instance;
+    private double position;
 
     private Climb() {
         climbMotor.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
@@ -27,12 +32,25 @@ public class Climb extends SubsystemBase {
         return instance;
     }
 
+    public void getPosition() {
+        position = this.climbEncoder.getPosition();
+        if (limitSwitchBottom.isPressed()) {
+            position = 0;
+        } else if (limitSwitchTop.isPressed()) {
+            position = ClimbConstants.MAX;
+        }
+    }
+
     public void climbUp() {
-        climbMotor.set(ClimbConstants.CLIMB_MOTOR_SPEED);
+        while (position < ClimbConstants.MAX) {
+            climbMotor.set(ClimbConstants.CLIMB_MOTOR_SPEED_UP);
+        }
     }
 
     public void climbDown() {
-        climbMotor.set(-(ClimbConstants.CLIMB_MOTOR_SPEED));
+        while (position > 0) {
+            climbMotor.set(-(ClimbConstants.CLIMB_MOTOR_SPEED_DOWN));
+        }
     }
 
     public void stopClimb() {
