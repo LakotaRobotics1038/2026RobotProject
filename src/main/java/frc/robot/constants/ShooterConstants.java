6c2786f11bd3c42b482b37a93f40299caa2d4eb1
murat <99989538+Murat65536@users.noreply.github.com>
2026-02-08 00:01:47 -0500

    Update to PID, change from max to flex, add constants, change to rpm control, and add SVA.

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -8,4 +8,12 @@ public final class ShooterConstants {
     public static final int FIRST_BOTTOM_SHOOTER_CAN_ID = 0;
     public static final int SECOND_TOP_SHOOTER_CAN_ID = 0;
     public static final int SECOND_BOTTOM_SHOOTER_CAN_ID = 0;
+
+    public static final double P = 0.0;
+    public static final double I = 0.0;
+    public static final double D = 0.0;
+
+    public static final double S = 0.0;
+    public static final double V = NeoMotorConstants.VOLTAGE / NeoMotorConstants.VORTEX_FREE_SPEED_RPM;
+    public static final double A = 0.0;
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,35 +1,57 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.NeoMotorConstants;
 import frc.robot.constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
-    private final SparkMax firstTopShooter = new SparkMax(ShooterConstants.FIRST_TOP_SHOOTER_CAN_ID,
+    private final SparkFlex firstShooter1 = new SparkFlex(ShooterConstants.FIRST_TOP_SHOOTER_CAN_ID,
             MotorType.kBrushless);
-    private final SparkMax firstBottomShooter = new SparkMax(ShooterConstants.FIRST_BOTTOM_SHOOTER_CAN_ID,
+    private final SparkFlex firstShooter2 = new SparkFlex(ShooterConstants.FIRST_BOTTOM_SHOOTER_CAN_ID,
             MotorType.kBrushless);
-    private final SparkMax secondTopShooter = new SparkMax(ShooterConstants.SECOND_TOP_SHOOTER_CAN_ID,
+    private final SparkFlex secondShooter1 = new SparkFlex(ShooterConstants.SECOND_TOP_SHOOTER_CAN_ID,
             MotorType.kBrushless);
-    private final SparkMax secondBottomShooter = new SparkMax(ShooterConstants.SECOND_BOTTOM_SHOOTER_CAN_ID,
+    private final SparkFlex secondShooter2 = new SparkFlex(ShooterConstants.SECOND_BOTTOM_SHOOTER_CAN_ID,
             MotorType.kBrushless);
+
+    private final SparkClosedLoopController firstController = firstShooter1.getClosedLoopController();
+    private final SparkClosedLoopController secondController = secondShooter1.getClosedLoopController();
+    private final RelativeEncoder firstEncoder = firstShooter1.getEncoder();
+    private final RelativeEncoder secondEncoder = secondShooter1.getEncoder();
 
     private static Shooter instance;
 
     private Shooter() {
-        firstTopShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-        firstBottomShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-        secondTopShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-        secondBottomShooter.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
-                PersistMode.kNoPersistParameters);
+        SparkFlexConfig baseConfig = new SparkFlexConfig();
+        baseConfig.smartCurrentLimit(NeoMotorConstants.MAX_VORTEX_CURRENT).closedLoop
+                .pid(ShooterConstants.P, ShooterConstants.I, ShooterConstants.D)
+                .outputRange(NeoMotorConstants.MIN_POWER, NeoMotorConstants.MAX_POWER).feedForward
+                .sva(ShooterConstants.S, ShooterConstants.V, ShooterConstants.A);
+        SparkFlexConfig firstConfig1 = new SparkFlexConfig();
+        firstConfig1.apply(baseConfig);
+        firstShooter1.configure(firstConfig1, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        SparkFlexConfig firstConfig2 = new SparkFlexConfig();
+        firstConfig2.apply(baseConfig).follow(firstShooter1, true);
+        firstShooter2.configure(firstConfig2, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        SparkFlexConfig secondConfig1 = new SparkFlexConfig();
+        secondConfig1.apply(baseConfig);
+        secondShooter1.configure(secondConfig1, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        SparkFlexConfig secondConfig2 = new SparkFlexConfig();
+        secondConfig2.apply(baseConfig).follow(secondShooter1, true);
+        secondShooter2.configure(secondConfig2, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
     }
 
     public static Shooter getInstance() {
@@ -39,23 +61,27 @@ public class Shooter extends SubsystemBase {
         return instance;
     }
 
-    public void runFirstShooter(double speed) {
-        firstTopShooter.set(speed);
-        firstBottomShooter.set(speed);
+    public void runFirstShooter(double rpm) {
+        firstController.setSetpoint(rpm, ControlType.kVelocity);
     }
 
-    public void runSecondShooter(double speed) {
-        secondTopShooter.set(speed);
-        secondBottomShooter.set(speed);
+    public void runSecondShooter(double rpm) {
+        secondController.setSetpoint(rpm, ControlType.kVelocity);
     }
 
     public void stopFirstShooter() {
-        firstTopShooter.stopMotor();
-        firstBottomShooter.stopMotor();
+        firstShooter1.stopMotor();
     }
 
     public void stopSecondShooter() {
-        secondTopShooter.stopMotor();
-        secondBottomShooter.stopMotor();
+        secondShooter1.stopMotor();
+    }
+
+    public double getFirstShooterRPM() {
+        return firstEncoder.getVelocity();
+    }
+
+    public double getSecondShooterRPM() {
+        return secondEncoder.getVelocity();
     }
 }
