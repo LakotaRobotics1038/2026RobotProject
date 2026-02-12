b4b302b1a03f2bc0b75e9add3c69ce7cf7f26161
murat <99989538+Murat65536@users.noreply.github.com>
2026-02-11 19:14:09 -0500

    Add hub align

---
+++ src/main/java/frc/robot/commands/HubAlign.java
@@ -0,0 +1,49 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.AutoConstants;
+import frc.robot.constants.DriveConstants;
+import frc.robot.constants.FieldConstants;
+import frc.robot.subsystems.DriveTrain;
+
+public class HubAlign extends Command {
+    private final DriveTrain driveTrain = DriveTrain.getInstance();
+    private final DoubleSupplier xSpeedSupplier;
+    private final DoubleSupplier ySpeedSupplier;
+    private final PIDController rotationController;
+
+    public HubAlign(DoubleSupplier xSpeed,
+            DoubleSupplier ySpeed) {
+        this.xSpeedSupplier = xSpeed;
+        this.ySpeedSupplier = ySpeed;
+
+        this.rotationController = new PIDController(
+                AutoConstants.P_THETA_CONTROLLER,
+                AutoConstants.I_THETA_CONTROLLER,
+                AutoConstants.D_THETA_CONTROLLER);
+
+        this.rotationController.enableContinuousInput(-Math.PI, Math.PI);
+
+        addRequirements(driveTrain);
+    }
+
+    @Override
+    public void execute() {
+        Translation2d currentTranslation = driveTrain.getState().Pose.getTranslation();
+        double targetAngleRad = Math.atan2(
+                FieldConstants.HUB_POSITION.getY() - currentTranslation.getY(),
+                FieldConstants.HUB_POSITION.getX() - currentTranslation.getX());
+
+        double currentRotationRadians = driveTrain.getState().Pose.getRotation().getRadians();
+        double rotationOutputRadPerSec = rotationController.calculate(currentRotationRadians, targetAngleRad);
+
+        driveTrain.setControl(driveTrain.drive(xSpeedSupplier.getAsDouble(), ySpeedSupplier.getAsDouble(),
+                MathUtil.clamp(rotationOutputRadPerSec / DriveConstants.MAX_ANGULAR_RATE, -1.0, 1.0),
+                true));
+    }
+}
---
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.constants;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class FieldConstants {
+    private FieldConstants() {
+    }
+
+    public static final Translation2d HUB_POSITION = new Translation2d(0, 0);
+}
