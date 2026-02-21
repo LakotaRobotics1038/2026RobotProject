b3dfe3a56003b8e97869ad895b9e431b964f803b
murat <99989538+Murat65536@users.noreply.github.com>
2026-02-21 14:20:31 -0500

    Make shooter great again

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,114 +4,141 @@ import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.servohub.ServoChannel;
+import com.revrobotics.servohub.ServoChannel.ChannelId;
 import com.revrobotics.servohub.ServoHub;
 import com.revrobotics.servohub.config.ServoChannelConfig;
 import com.revrobotics.servohub.config.ServoHubConfig;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.*;
 import com.revrobotics.spark.config.SparkFlexConfig;
-
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.NeoMotorConstants;
 import frc.robot.constants.ShooterConstants;
 
-/**
- * Base shooter subsystem.
- */
-public abstract class Shooter extends SubsystemBase {
-    private final SparkFlex leftMotor;
-    private final SparkFlex rightMotor;
-    private final SparkClosedLoopController controller;
-    private final RelativeEncoder encoder;
-    private final Translation3d translation;
-    private static ServoHub servoHub;
+public class Shooter extends SubsystemBase {
+    private static Shooter instance;
+
+    private final NearShooter nearShooter;
+    private final FarShooter farShooter;
+
+    private final ServoHub servoHub;
+
+    private Shooter() {
+        servoHub = new ServoHub(ShooterConstants.SERVO_HUB_CAN_ID);
+        ServoHubConfig config = new ServoHubConfig();
+        nearShooter = new NearShooter(config);
+        farShooter = new FarShooter(config);
+        servoHub.configure(config, ResetMode.kResetSafeParameters);
+    }
+
+    public static Shooter getInstance() {
+        if (instance == null) {
+            instance = new Shooter();
+        }
+        return instance;
+    }
+
+    private class NearShooter extends ShooterTemplate {
+        private NearShooter(ServoHubConfig config) {
+            super(ShooterConstants.NEAR_LEFT_MOTOR_CAN_ID, ShooterConstants.NEAR_RIGHT_MOTOR_CAN_ID, ShooterConstants.NEAR_TRANSLATION, ShooterConstants.NEAR_SERVO_CHANNEL);
+            config.apply(ShooterConstants.NEAR_SERVO_CHANNEL, new ServoChannelConfig(ShooterConstants.NEAR_SERVO_CHANNEL).pulseRange(ShooterConstants.NEAR_SERVO_PULSE_RANGE));
+        }
+    }
+
+    private class FarShooter extends ShooterTemplate {
+        private FarShooter(ServoHubConfig config) {
+            super(ShooterConstants.FAR_LEFT_MOTOR_CAN_ID, ShooterConstants.FAR_RIGHT_MOTOR_CAN_ID, ShooterConstants.FAR_TRANSLATION, ShooterConstants.FAR_SERVO_CHANNEL);
+            config.apply(ShooterConstants.FAR_SERVO_CHANNEL, new ServoChannelConfig(ShooterConstants.FAR_SERVO_CHANNEL).pulseRange(ShooterConstants.FAR_SERVO_PULSE_RANGE));
+        }
+    }
 
     /**
-     * Creates a shooter with the specified motor controller CAN IDs.
-     *
-     * @param leftMotorCanId  CAN ID of the left shooter motor controller.
-     * @param rightMotorCanId CAN ID of the right shooter motor controller.
+     * Base shooter template.
      */
-    protected Shooter(int leftMotorCanId, int rightMotorCanId, Translation3d translation) {
-        if (servoHub == null) {
-            servoHub = new ServoHub(ShooterConstants.SERVO_HUB_CAN_ID);
-            ServoHubConfig config = new ServoHubConfig();
-            config.apply(ShooterConstants.NEAR_SERVO_CHANNEL, new ServoChannelConfig(ShooterConstants.NEAR_SERVO_CHANNEL).pulseRange(ShooterConstants.NEAR_SERVO_PULSE_RANGE));
-            config.apply(ShooterConstants.FAR_SERVO_CHANNEL, new ServoChannelConfig(ShooterConstants.FAR_SERVO_CHANNEL).pulseRange(ShooterConstants.FAR_SERVO_PULSE_RANGE));
-            servoHub.configure(config, ResetMode.kResetSafeParameters);
+    private abstract class ShooterTemplate {
+        private final SparkFlex leftMotor;
+        private final SparkFlex rightMotor;
+        private final SparkClosedLoopController controller;
+        private final RelativeEncoder encoder;
+        private final Translation3d translation;
+        private final ChannelId servoChannel;
+
+        /**
+         * Creates a shooter with the specified motor controller CAN IDs.
+         *
+         * @param leftMotorCanId  CAN ID of the left shooter motor controller.
+         * @param rightMotorCanId CAN ID of the right shooter motor controller.
+         */
+        protected ShooterTemplate(int leftMotorCanId, int rightMotorCanId, Translation3d translation, ChannelId servoChannel) {
+            SparkFlexConfig baseConfig = new SparkFlexConfig();
+            baseConfig.smartCurrentLimit(NeoMotorConstants.MAX_VORTEX_CURRENT).closedLoop
+                    .pid(ShooterConstants.P, ShooterConstants.I, ShooterConstants.D)
+                    .allowedClosedLoopError(ShooterConstants.RPM_TOLERANCE, ClosedLoopSlot.kSlot0)
+                    .outputRange(NeoMotorConstants.MIN_POWER, NeoMotorConstants.MAX_POWER).feedForward
+                    .sva(ShooterConstants.S, ShooterConstants.V, ShooterConstants.A);
+
+            SparkFlexConfig leftMotorConfig = new SparkFlexConfig();
+            leftMotorConfig.apply(baseConfig);
+            leftMotor = new SparkFlex(leftMotorCanId, SparkLowLevel.MotorType.kBrushless);
+            leftMotor.configure(leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+            SparkFlexConfig rightMotorConfig = new SparkFlexConfig();
+            rightMotorConfig.apply(baseConfig).follow(leftMotor, true);
+            rightMotor = new SparkFlex(rightMotorCanId, SparkLowLevel.MotorType.kBrushless);
+            rightMotor.configure(rightMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+            controller = leftMotor.getClosedLoopController();
+            encoder = leftMotor.getEncoder();
+
+            this.translation = translation;
+            this.servoChannel = servoChannel;
         }
 
-        SparkFlexConfig baseConfig = new SparkFlexConfig();
-        baseConfig.smartCurrentLimit(NeoMotorConstants.MAX_VORTEX_CURRENT).closedLoop
-                .pid(ShooterConstants.P, ShooterConstants.I, ShooterConstants.D)
-                .allowedClosedLoopError(ShooterConstants.RPM_TOLERANCE, ClosedLoopSlot.kSlot0)
-                .outputRange(NeoMotorConstants.MIN_POWER, NeoMotorConstants.MAX_POWER).feedForward
-                .sva(ShooterConstants.S, ShooterConstants.V, ShooterConstants.A);
+        /**
+         * Starts the shooter at an RPM.
+         *
+         * @param rpm Target shooter speed in RPM.
+         */
+        public void start(double rpm) {
+            controller.setSetpoint(rpm, SparkBase.ControlType.kVelocity);
+        }
 
-        SparkFlexConfig leftMotorConfig = new SparkFlexConfig();
-        leftMotorConfig.apply(baseConfig);
-        leftMotor = new SparkFlex(leftMotorCanId, MotorType.kBrushless);
-        leftMotor.configure(leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        /**
+         * Stops the shooter.
+         */
+        public void stop() {
+            leftMotor.stopMotor();
+            rightMotor.stopMotor();
+        }
 
-        SparkFlexConfig rightMotorConfig = new SparkFlexConfig();
-        rightMotorConfig.apply(baseConfig).follow(leftMotor, true);
-        rightMotor = new SparkFlex(rightMotorCanId, MotorType.kBrushless);
-        rightMotor.configure(rightMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        /**
+         * Returns the current shooter speed.
+         *
+         * @return Current shooter speed in RPM.
+         */
+        public double getRPM() {
+            return encoder.getVelocity();
+        }
 
-        controller = leftMotor.getClosedLoopController();
-        encoder = leftMotor.getEncoder();
+        public double getTargetRPM() {
+            return controller.getSetpoint();
+        }
 
-        this.translation = translation;
-    }
+        /**
+         * Indicates whether the shooter at its target RPM.
+         *
+         * @return Whether the shooter is at the target RPM.
+         */
+        public boolean isAtTargetRPM() {
+            return controller.isAtSetpoint();
+        }
 
-    /**
-     * Starts the shooter at an RPM.
-     *
-     * @param rpm Target shooter speed in RPM.
-     */
-    public void start(double rpm) {
-        controller.setSetpoint(rpm, ControlType.kVelocity);
-    }
+        public Translation3d getTranslation() {
+            return translation;
+        }
 
-    /**
-     * Stops the shooter.
-     */
-    public void stop() {
-        leftMotor.stopMotor();
-        rightMotor.stopMotor();
-    }
-
-    /**
-     * Returns the current shooter speed.
-     *
-     * @return Current shooter speed in RPM.
-     */
-    public double getRPM() {
-        return encoder.getVelocity();
-    }
-
-    public double getTargetRPM() {
-        return controller.getSetpoint();
-    }
-
-    /**
-     * Indicates whether the shooter at its target RPM.
-     *
-     * @return Whether the shooter is at the target RPM.
-     */
-    public boolean isAtTargetRPM() {
-        return controller.isAtSetpoint();
-    }
-
-    public Translation3d getTranslation() {
-        return translation;
-    }
-
-    public ServoHub getServoHub() {
-        return servoHub;
+        public ServoChannel getServoChannel() {
+            return servoHub.getServoChannel(servoChannel);
+        }
     }
 }
