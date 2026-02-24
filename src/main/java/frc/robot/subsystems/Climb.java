f338698486a5a4b33d4ddb90ac7e2c5cc9e2e216
Murat <>
2026-02-24 08:04:36 -1000

    Add isAtSetpoint

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
@@ -15,16 +13,14 @@ import frc.robot.constants.ClimbConstants;
 
 public class Climb extends SubsystemBase {
 
-    private final SparkMax climbMotor = new SparkMax(ClimbConstants.CLIMB_MOTOR_CAN_ID,
+    private final SparkMax motor = new SparkMax(ClimbConstants.CLIMB_MOTOR_CAN_ID,
             MotorType.kBrushless);
-    private final SparkClosedLoopController climbController = climbMotor.getClosedLoopController();
-    private final AbsoluteEncoder climbEncoder = climbMotor.getAbsoluteEncoder();
+    private final SparkClosedLoopController controller = motor.getClosedLoopController();
     private static Climb instance;
-    private double position;
 
     private Climb() {
 
-        climbMotor.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
+        motor.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
                 PersistMode.kNoPersistParameters);
     }
 
@@ -36,14 +32,18 @@ public class Climb extends SubsystemBase {
     }
 
     public void climbUp() {
-        climbController.setSetpoint(ClimbConstants.MAX_CLIMB, ControlType.kPosition);
+        controller.setSetpoint(ClimbConstants.MAX_CLIMB, ControlType.kPosition);
     }
 
     public void climbDown() {
-        climbController.setSetpoint(ClimbConstants.MIN_CLIMB, ControlType.kPosition);
+        controller.setSetpoint(ClimbConstants.MIN_CLIMB, ControlType.kPosition);
+    }
+
+    public boolean isAtSetpoint() {
+        return controller.isAtSetpoint();
     }
 
     public void stopClimb() {
-        climbMotor.stopMotor();
+        motor.stopMotor();
     }
 }
