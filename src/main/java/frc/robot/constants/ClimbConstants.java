82257d0a174747351331175f6a09b6cdd428fbc4
Myles A. Knowlin <117530083+Makinist08@users.noreply.github.com>
2026-02-23 19:56:42 -0500

    Still need PID and getters, no while loops in robot code

--- src/main/java/frc/robot/constants/ClimbConstants.java
+++ src/main/java/frc/robot/constants/ClimbConstants.java
@@ -7,5 +7,6 @@ public final class ClimbConstants {
     public static final int CLIMB_MOTOR_CAN_ID = 0;
     public static final double CLIMB_MOTOR_SPEED_UP = 0;
     public static final double CLIMB_MOTOR_SPEED_DOWN = 0;
-    public static final double MAX = 0;
+    public static final double MAX_CLIMB = 0;
+    public static final double MIN_CLIMB = 0;
 }
--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
@@ -13,12 +15,15 @@ import frc.robot.constants.ClimbConstants;
 
 public class Climb extends SubsystemBase {
 
-    private final SparkMax climbMotor = new SparkMax(ClimbConstants.CLIMB_MOTOR_CAN_ID, MotorType.kBrushless);
+    private final SparkMax climbMotor = new SparkMax(ClimbConstants.CLIMB_MOTOR_CAN_ID,
+            MotorType.kBrushless);
+    private final SparkClosedLoopController climbController = climbMotor.getClosedLoopController();
     private final AbsoluteEncoder climbEncoder = climbMotor.getAbsoluteEncoder();
     private static Climb instance;
     private double position;
 
     private Climb() {
+
         climbMotor.configure(SparkMaxConfig.Presets.REV_NEO, ResetMode.kResetSafeParameters,
                 PersistMode.kNoPersistParameters);
     }
@@ -31,15 +36,11 @@ public class Climb extends SubsystemBase {
     }
 
     public void climbUp() {
-        while (position < ClimbConstants.MAX) {
-            climbMotor.set(ClimbConstants.CLIMB_MOTOR_SPEED_UP);
-        }
+        climbController.setSetpoint(ClimbConstants.MAX_CLIMB, ControlType.kPosition);
     }
 
     public void climbDown() {
-        while (position > 0) {
-            climbMotor.set(-(ClimbConstants.CLIMB_MOTOR_SPEED_DOWN));
-        }
+        climbController.setSetpoint(ClimbConstants.MIN_CLIMB, ControlType.kPosition);
     }
 
     public void stopClimb() {
