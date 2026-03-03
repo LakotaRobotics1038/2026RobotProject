9e54f76feee9de9904aac098abf612d5fc038119
Murat <>
2026-03-03 13:56:35 -1000

    Add can ids

--- src/main/java/frc/robot/constants/KickerConstants.java
+++ src/main/java/frc/robot/constants/KickerConstants.java
@@ -4,7 +4,7 @@ public class KickerConstants {
     private KickerConstants() {
     }
 
-    public static final int CAN_ID = 0;
+    public static final int CAN_ID = 14;
 
     public static final double P = 0.0;
     public static final double I = 0.0;
