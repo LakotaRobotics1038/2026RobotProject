6e411cb24fdc7698ad90042c4810736125440515
Myles A. Knowlin <117530083+Makinist08@users.noreply.github.com>
2026-03-07 13:24:56 -0500

    Merged PivotAcqCommand

--- src/main/java/frc/robot/commands/PivotAcquisitionCommand.java
+++ src/main/java/frc/robot/commands/PivotAcquisitionCommand.java
@@ -1,13 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.AcquisitionConstants.AcquisitionSetpoint;
 import frc.robot.subsystems.Acquisition;
 
 public class PivotAcquisitionCommand extends Command {
     private final Acquisition acquisition = Acquisition.getInstance();
-    private final Acquisition.AcquisitionSetpoint setpoint;
+    private final AcquisitionSetpoint setpoint;
 
-    public PivotAcquisitionCommand(Acquisition.AcquisitionSetpoint setpoint) {
+    public PivotAcquisitionCommand(AcquisitionSetpoint setpoint) {
         addRequirements(acquisition);
         this.setpoint = setpoint;
     }
