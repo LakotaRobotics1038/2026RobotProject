b69b8e0b4b8af3a170eff83241fb06a1ef6a51f8
Murat <>
2026-03-10 11:42:05 -0900

    Add acquisition pivot and run

--- src/main/java/frc/robot/OperatorJoystick.java
+++ src/main/java/frc/robot/OperatorJoystick.java
@@ -1,7 +1,10 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.AcquisitionPivotCommand;
+import frc.robot.commands.AcquisitionRunCommand;
 import frc.robot.commands.ClimbCommand;
+import frc.robot.constants.AcquisitionConstants;
 import frc.robot.constants.ClimbConstants;
 import frc.robot.constants.IOConstants;
 import frc.robot.libraries.XboxController1038;
@@ -19,7 +22,15 @@ public class OperatorJoystick extends XboxController1038 {
     private OperatorJoystick() {
         super(IOConstants.OPERATOR_CONTROLLER_PORT);
 
-        this.leftBumper().onTrue(new ClimbCommand(ClimbConstants.ClimbSetpoint.DOWN));
-        this.rightBumper().onTrue(new ClimbCommand(ClimbConstants.ClimbSetpoint.UP));
+        new Trigger(() -> this.getPOV().equals(PovPositions.Up))
+                .onTrue(new ClimbCommand(ClimbConstants.ClimbSetpoint.UP));
+        new Trigger(() -> this.getPOV().equals(PovPositions.Down))
+                .onTrue(new ClimbCommand(ClimbConstants.ClimbSetpoint.DOWN));
+
+        this.leftBumper().whileTrue(new AcquisitionRunCommand(AcquisitionRunCommand.Mode.DISPOSE));
+        this.rightBumper().whileTrue(new AcquisitionRunCommand(AcquisitionRunCommand.Mode.INTAKE));
+
+        this.y().onTrue(new AcquisitionPivotCommand(AcquisitionConstants.AcquisitionSetpoint.RAISED));
+        this.a().onTrue(new AcquisitionPivotCommand(AcquisitionConstants.AcquisitionSetpoint.LOWERED));
     }
 }
