069ae0f4f7cb86d0cc7244b2df7fe3c53a25bfc2
Myles A. Knowlin <117530083+Makinist08@users.noreply.github.com>
2026-03-14 14:19:04 -0400

    Neutral no climb written

--- src/main/java/frc/robot/autons/OutpostNeutralNoClimb.java
+++ src/main/java/frc/robot/autons/OutpostNeutralNoClimb.java
@@ -8,16 +8,29 @@ import org.json.simple.parser.ParseException;
 import com.pathplanner.lib.util.FileVersionException;
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.AcquisitionPivotCommand;
+import frc.robot.commands.AcquisitionRunCommand;
+import frc.robot.commands.AutoShootCommand;
+import frc.robot.constants.AcquisitionConstants.AcquisitionSetpoint;
 
 public class OutpostNeutralNoClimb extends OutpostNeutral {
     public OutpostNeutralNoClimb(Optional<Alliance> alliance)
             throws FileVersionException, IOException, ParseException {
         super(alliance);
         super.addCommands(
-        /**
-         * TODO 4 collect from some where
-         * TODO 5 shoot from some where
-         */
+
+        // TODO 4 acquire from neutral again ig
+        // followPathCommand(Paths.getOutpostShootingToNeutral()),
+        // new AcquisitionPivotCommand(AcquisitionSetpoint.LOWERED)
+        // .andThen(new AcquisitionRunCommand(AcquisitionRunCommand.Mode.INTAKE)
+        // .raceWith(followPathCommand(Paths.getOutpostAcquireFromNeutral())
+        // .andThen(new WaitCommand(0.5)))),
+        // new AcquisitionPivotCommand(AcquisitionSetpoint.RAISED),
+
+        // TODO 5 shooting again
+        // followPathCommand(Paths.getOutpostNeutralToShooting()),
+        // new AutoShootCommand().withDeadline(new WaitCommand(2))
         );
 
     }
