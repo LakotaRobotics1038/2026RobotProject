8736e4ae916d6f3bab8cea699c17bf88f8c0054e
Wes Reed <reedws@ucmail.uc.edu>
2026-03-17 20:59:15 -0400

    code builds now

--- src/main/java/frc/robot/autons/DepotNeutral.java
+++ src/main/java/frc/robot/autons/DepotNeutral.java
@@ -10,7 +10,7 @@ import com.pathplanner.lib.util.FileVersionException;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.commands.AcquisitionPivotCommand;
 import frc.robot.commands.AcquisitionRunCommand;
-import frc.robot.commands.AutoShootCommand;
+import frc.robot.commands.ShootCommand;
 import frc.robot.constants.AcquisitionConstants.AcquisitionSetpoint;
 
 public class DepotNeutral extends Auton {
@@ -18,12 +18,12 @@ public class DepotNeutral extends Auton {
         super(alliance);
         super.addCommands(
                 followPathCommand(Paths.getMiddleToShootingPath()),
-                new AutoShootCommand().withTimeout(2),
+                new ShootCommand().withTimeout(2),
                 followPathCommand(Paths.getShootingToDepotPath()).alongWith(
                         new AcquisitionPivotCommand(AcquisitionSetpoint.LOWERED)),
                 new AcquisitionRunCommand(AcquisitionRunCommand.Mode.INTAKE)
                         .raceWith(followPathCommand(Paths.getAcquireFromDepotPath())),
                 followPathCommand(Paths.getDepotToShootingPath()),
-                new AutoShootCommand().withTimeout(2));
+                new ShootCommand().withTimeout(2));
     }
 }
--- src/main/java/frc/robot/autons/OutpostNeutral.java
+++ src/main/java/frc/robot/autons/OutpostNeutral.java
@@ -10,7 +10,7 @@ import com.pathplanner.lib.util.FileVersionException;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.commands.AcquisitionPivotCommand;
 import frc.robot.commands.AcquisitionRunCommand;
-import frc.robot.commands.AutoShootCommand;
+import frc.robot.commands.ShootCommand;
 import frc.robot.constants.AcquisitionConstants.AcquisitionSetpoint;
 
 public class OutpostNeutral extends Auton {
@@ -18,12 +18,12 @@ public class OutpostNeutral extends Auton {
         super(alliance);
         super.addCommands(
                 followPathCommand(Paths.getDepotStartToShootingPath()),
-                new AutoShootCommand().withTimeout(2),
+                new ShootCommand().withTimeout(2),
                 followPathCommand(Paths.getDepotShootingToNeutralPath()).alongWith(
                         new AcquisitionPivotCommand(AcquisitionSetpoint.LOWERED)),
                 new AcquisitionRunCommand(AcquisitionRunCommand.Mode.INTAKE)
                         .raceWith(followPathCommand(Paths.getAcquireFromDepotPath())),
                 followPathCommand(Paths.getDepotNeutralToAcquirePath()),
-                new AutoShootCommand().withTimeout(2));
+                new ShootCommand().withTimeout(2));
     }
 }
