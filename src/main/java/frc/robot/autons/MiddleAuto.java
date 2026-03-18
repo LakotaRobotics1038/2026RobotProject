cbf2d47e61bc7adc6b7d4d846896eb1f429676dc
Murat <>
2026-03-17 17:57:19 -0900

    Finish middle auton

---
+++ src/main/java/frc/robot/autons/MiddleAuto.java
@@ -0,0 +1,31 @@
+package frc.robot.autons;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.util.FileVersionException;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.commands.AcquisitionPivotCommand;
+import frc.robot.commands.AcquisitionRunCommand;
+import frc.robot.commands.ShootCommand;
+import frc.robot.commands.AcquisitionRunCommand.Mode;
+import frc.robot.constants.AcquisitionConstants.AcquisitionSetpoint;
+
+public class MiddleAuto extends Auton {
+    public MiddleAuto(Optional<Alliance> alliance) throws FileVersionException, IOException, ParseException {
+        super(alliance);
+        super.addCommands(
+                followPathCommand(Paths.getMiddle1Path()),
+                followPathCommand(Paths.getMiddle2Path()).alongWith(
+                        new AcquisitionPivotCommand(AcquisitionSetpoint.LOWERED),
+                        new AcquisitionRunCommand(Mode.INTAKE)),
+                followPathCommand(Paths.getMiddle3Path()).alongWith(new AcquisitionRunCommand(Mode.STOP)),
+                new ShootCommand().withTimeout(2),
+                followPathCommand(Paths.getMiddle4Path()).alongWith(new AcquisitionRunCommand(Mode.INTAKE)),
+                followPathCommand(Paths.getMiddle5Path()).alongWith(new AcquisitionRunCommand(Mode.STOP)),
+                new ShootCommand().withTimeout(2));
+    }
+}
