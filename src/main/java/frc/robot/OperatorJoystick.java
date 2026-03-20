b0d83a90005766b8bc25a7768209bdbfef803480
Murat <>
2026-03-20 07:58:16 -0900

    Cleanup

--- src/main/java/frc/robot/OperatorJoystick.java
+++ src/main/java/frc/robot/OperatorJoystick.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.AcquisitionPivotCommand;
@@ -10,13 +9,10 @@ import frc.robot.commands.ShootCommand;
 import frc.robot.constants.*;
 import frc.robot.libraries.XboxController1038;
 import frc.robot.subsystems.Dashboard;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.utils.RectangleUtils;
 
 public class OperatorJoystick extends XboxController1038 {
     public static OperatorJoystick instance;
     private final Dashboard dashboard = Dashboard.getInstance();
-    private final DriveTrain driveTrain = DriveTrain.getInstance();
 
     public static OperatorJoystick getInstance() {
         if (instance == null) {
@@ -52,22 +48,6 @@ public class OperatorJoystick extends XboxController1038 {
             dashboard.resetManualShooterHoodAngle();
         }));
 
-        // new Trigger(this::isInTrench)
-        // .and(() -> DriverStation.isTeleopEnabled())
-        // .and(() -> !dashboard.isManualModeEnabled())
-        // .onTrue(new AcquisitionTrenchRetract());
-
-        // new Trigger(this::isInTrench)
-        // .and(() -> !dashboard.isManualModeEnabled())
-        // .whileTrue(new RetractHoodsCommand());
-
         this.rightTrigger().whileTrue(new ShootCommand(() -> this.b().getAsBoolean()));
     }
-
-    // private boolean isInTrench() {
-    // Translation2d robotPos = driveTrain.getState().Pose.getTranslation();
-    // return RectangleUtils.inRect(
-    // FieldConstants.TRENCH_RECTANGLES,
-    // robotPos);
-    // }
 }
