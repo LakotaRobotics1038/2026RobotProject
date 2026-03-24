5aad10ecc30af8827174886dc7f9057690a95ddd
murat <99989538+Murat65536@users.noreply.github.com>
2026-03-24 18:45:21 -0400

    Remove wildcard

--- src/main/java/frc/robot/autons/RightAutoShoot.java
+++ src/main/java/frc/robot/autons/RightAutoShoot.java
@@ -2,7 +2,10 @@ package frc.robot.autons;
 
 import com.pathplanner.lib.util.FileVersionException;
 import edu.wpi.first.wpilibj.DriverStation;
-import frc.robot.commands.*;
+import frc.robot.commands.AcquisitionPivotCommand;
+import frc.robot.commands.AdjustHoodsCommand;
+import frc.robot.commands.AlignCommand;
+import frc.robot.commands.ShootCommand;
 import frc.robot.constants.AcquisitionConstants;
 import org.json.simple.parser.ParseException;
 
