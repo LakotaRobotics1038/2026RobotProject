7861886ed8712e10281040dbcac77894d26fa93f
murat <99989538+Murat65536@users.noreply.github.com>
2026-03-25 17:20:20 -0400

    Fix middle auto

--- src/main/java/frc/robot/autons/MiddleAuto.java
+++ src/main/java/frc/robot/autons/MiddleAuto.java
@@ -19,9 +19,9 @@ public class MiddleAuto extends Auton {
         super(alliance);
         super.addCommands(
                 followPathCommand(Paths.getMiddle1Path()),
-                followPathCommand(Paths.getMiddle2Path()).alongWith(
+                followPathCommand(Paths.getMiddle2Path()).raceWith(
                         new AcquisitionPivotCommand(AcquisitionSetpoint.LOWERED)
-                                .andThen(new AcquisitionRunCommand(Mode.INTAKE).withTimeout(2))),
+                                .andThen(new AcquisitionRunCommand(Mode.INTAKE))),
                 followPathCommand(Paths.getMiddle3Path()),
                 new ShootCommand().withTimeout(2),
                 followPathCommand(Paths.getMiddle4Path()).raceWith(new AcquisitionRunCommand(Mode.INTAKE)),
