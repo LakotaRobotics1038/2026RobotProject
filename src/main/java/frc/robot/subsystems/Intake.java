295515304f9c0baabeffce1569dba63c3038d366
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-03-28 08:38:28 -0900

    Make 2 motors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,12 +12,20 @@ import frc.robot.constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
     private static Intake instance;
-    private final SparkFlex motor = new SparkFlex(IntakeConstants.MOTOR_CAN_ID, SparkFlex.MotorType.kBrushless);
+    private final SparkFlex leftMotor = new SparkFlex(IntakeConstants.LEFT_MOTOR_CAN_ID,
+            SparkFlex.MotorType.kBrushless);
+    private final SparkFlex rightMotor = new SparkFlex(IntakeConstants.RIGHT_MOTOR_CAN_ID,
+            SparkFlex.MotorType.kBrushless);
+    private final SparkClosedLoopController controller = leftMotor.getClosedLoopController();
 
     private Intake() {
         SparkFlexConfig config = new SparkFlexConfig();
         config.idleMode(SparkFlexConfig.IdleMode.kCoast);
-        motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        leftMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        SparkFlexConfig followerConfig = new SparkFlexConfig();
+        followerConfig.apply(config).follow(leftMotor, true);
+        rightMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     public static Intake getInstance() {
@@ -26,18 +36,18 @@ public class Intake extends SubsystemBase {
     }
 
     public void forward() {
-        motor.set(IntakeConstants.FORWARD_POWER);
+        controller.setSetpoint(IntakeConstants.FORWARD_POWER, ControlType.kDutyCycle);
     }
 
     public void backward() {
-        motor.set(IntakeConstants.BACKWARD_POWER);
+        controller.setSetpoint(IntakeConstants.BACKWARD_POWER, ControlType.kDutyCycle);
     }
 
     public void stop() {
-        motor.stopMotor();
+        leftMotor.stopMotor();
     }
 
     public double getRPM() {
-        return motor.getEncoder().getVelocity();
+        return leftMotor.getEncoder().getVelocity();
     }
 }
