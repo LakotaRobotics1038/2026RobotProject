6f65012ab28482307aff9292c579c2d6f8e2a403
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-03-28 08:31:03 -0900

    Change indexer can id

--- src/main/java/frc/robot/constants/IndexerConstants.java
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.constants;
 
 public final class IndexerConstants {
-    public static final int MOTOR_CAN_ID = 0;
+    public static final int MOTOR_CAN_ID = 3;
 
     public static final double FORWARD_POWER = 0.3;
     public static final double BACKWARD_POWER = -0.3;
