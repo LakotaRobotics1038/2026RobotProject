76ac94d5d98bf298262b2876d1df8bf35a7c33b1
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-03-30 13:43:34 -0400

    Change can id

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.constants;
 
 public final class AcquisitionConstants {
-    public static final int MOTOR_CAN_ID = 15;
+    public static final int MOTOR_CAN_ID = 7;
 
     public static final double ACQUIRE_SPEED = 1.0;
     public static final double DISPOSE_SPEED = -0.5;
