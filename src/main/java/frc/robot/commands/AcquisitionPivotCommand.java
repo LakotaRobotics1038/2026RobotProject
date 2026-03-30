0355eb5cf3028c469c56b4ae7cdba13af79e40da
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-03-30 13:28:49 -0400

    Change to pivot

--- src/main/java/frc/robot/commands/AcquisitionPivotCommand.java
+++ src/main/java/frc/robot/commands/AcquisitionPivotCommand.java
@@ -5,21 +5,21 @@ import frc.robot.constants.AcquisitionPivotConstants.PivotSetpoint;
 import frc.robot.subsystems.AcquisitionPivot;
 
 public class AcquisitionPivotCommand extends Command {
-    private final AcquisitionPivot acquisition = AcquisitionPivot.getInstance();
+    private final AcquisitionPivot pivot = AcquisitionPivot.getInstance();
     private final PivotSetpoint setpoint;
 
     public AcquisitionPivotCommand(PivotSetpoint setpoint) {
         this.setpoint = setpoint;
-        addRequirements(acquisition);
+        addRequirements(pivot);
     }
 
     @Override
     public void initialize() {
-        acquisition.setAngleSetpoint(setpoint);
+        pivot.setAngleSetpoint(setpoint);
     }
 
     @Override
     public boolean isFinished() {
-        return acquisition.isAtSetpoint();
+        return pivot.isAtSetpoint();
     }
 }
