55ce25b46bf3125c93f632c66d21a78d53737fca
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-03-30 21:58:00 -0400

    Fix syntax

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.autons.AutonSelector.AutonChoices;
+import frc.robot.autons.AutonSelector.AutonChoice;
 import frc.robot.constants.AcquisitionPivotConstants;
 import frc.robot.constants.DashboardConstants;
 import frc.robot.constants.ShooterConstants;
