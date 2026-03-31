d834c4514f0411368e031e9e0c847b9d41d2b798
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-03-31 14:38:08 -0400

    Change right auto

--- src/main/java/frc/robot/autons/RightAutoShoot.java
+++ src/main/java/frc/robot/autons/RightAutoShoot.java
@@ -6,7 +6,6 @@ import frc.robot.commands.AcquisitionPivotCommand;
 import frc.robot.commands.AdjustHoodsCommand;
 import frc.robot.commands.AlignCommand;
 import frc.robot.commands.ShootCommand;
-import frc.robot.constants.AcquisitionPivotConstants;
 import frc.robot.constants.AcquisitionPivotConstants.PivotSetpoint;
 
 import org.json.simple.parser.ParseException;
