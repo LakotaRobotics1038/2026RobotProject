bf9aa910ae87b9135b84cdb2180bed86af54b76c
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-04-20 21:47:07 -0400

    Update shooter hood

--- src/main/java/frc/robot/constants/ShooterHoodConstants.java
+++ src/main/java/frc/robot/constants/ShooterHoodConstants.java
@@ -4,21 +4,16 @@ public final class ShooterHoodConstants {
     public static final int LEFT_MOTOR_CAN_ID = 13;
     public static final int RIGHT_MOTOR_CAN_ID = 12;
 
-    public static final double P = 0;
-    public static final double I = 0;
+    public static final double P = 16;
+    public static final double I = 0.005;
     public static final double D = 0;
-    public static final double S = 0;
-    public static final double V = NeoMotorConstants.BATTERY_VOLTAGE / NeoMotorConstants.NEO_550_FREE_SPEED_RPM;
-    public static final double A = 0;
 
-    public static final double HOOD_ENCODER_CONVERSION_FACTOR = 360;
+    public static final double SHOOTER_NO_RETRACTION_ANGLE = 50.0;
+    public static final double SHOOTER_FULL_RETRACTION_ANGLE = 76.0;
 
-    public static final double SHOOTER_NO_RETRACTION_ANGLE = 54.0;
-    public static final double SHOOTER_FULL_RETRACTION_ANGLE = 73.0;
+    public static final double HOOD_ENCODER_CONVERSION_FACTOR = (SHOOTER_FULL_RETRACTION_ANGLE
+            - SHOOTER_NO_RETRACTION_ANGLE) * 2;
 
-    public static final double MANUAL_SHOOTER_DEFAULT_ANGLE = 59;
+    public static final double MANUAL_SHOOTER_DEFAULT_ANGLE = 55;
     public static final double MANUAL_SHOOTER_ANGLE_INCREMENT = 2;
-
-    public static final double MIN_ANGLE = 43.517;
-    public static final double MAX_ANGLE = 67.255;
 }
--- src/main/java/frc/robot/subsystems/ShooterHood.java
+++ src/main/java/frc/robot/subsystems/ShooterHood.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
@@ -11,6 +13,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.NeoMotorConstants;
 import frc.robot.constants.ShooterHoodConstants;
 
 public class ShooterHood extends SubsystemBase {
@@ -24,13 +27,17 @@ public class ShooterHood extends SubsystemBase {
 
     private ShooterHood() {
         SparkMaxConfig baseConfig = new SparkMaxConfig();
-        baseConfig.idleMode(SparkMaxConfig.IdleMode.kBrake).closedLoop.pid(ShooterHoodConstants.P,
-                ShooterHoodConstants.I, ShooterHoodConstants.D).feedForward
-                .sva(ShooterHoodConstants.S, ShooterHoodConstants.V, ShooterHoodConstants.A);
+        baseConfig.smartCurrentLimit(NeoMotorConstants.MAX_NEO_550_CURRENT)
+                .idleMode(SparkMaxConfig.IdleMode.kBrake).closedLoop.pid(ShooterHoodConstants.P,
+                        ShooterHoodConstants.I, ShooterHoodConstants.D);
 
         SparkMaxConfig leftMotorConfig = new SparkMaxConfig();
-        leftMotorConfig.apply(baseConfig).inverted(true).absoluteEncoder
+        leftMotorConfig.apply(baseConfig).inverted(true).absoluteEncoder.inverted(true)
                 .positionConversionFactor(ShooterHoodConstants.HOOD_ENCODER_CONVERSION_FACTOR);
+        leftMotorConfig.closedLoop.positionWrappingEnabled(true).positionWrappingInputRange(0,
+                ShooterHoodConstants.HOOD_ENCODER_CONVERSION_FACTOR).feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+                .allowedClosedLoopError(0.5, ClosedLoopSlot.kSlot0);
+
         leftMotor.configure(leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         SparkMaxConfig rightMotorConfig = new SparkMaxConfig();
@@ -46,11 +53,14 @@ public class ShooterHood extends SubsystemBase {
     }
 
     public void setAngle(double angle) {
-        controller.setSetpoint(MathUtil.clamp(angle, ShooterHoodConstants.MIN_ANGLE, ShooterHoodConstants.MAX_ANGLE),
+        controller.setSetpoint(
+                ShooterHoodConstants.SHOOTER_FULL_RETRACTION_ANGLE
+                        - MathUtil.clamp(angle, ShooterHoodConstants.SHOOTER_NO_RETRACTION_ANGLE,
+                                ShooterHoodConstants.SHOOTER_FULL_RETRACTION_ANGLE),
                 ControlType.kPosition);
     }
 
     public double getAngle() {
-        return encoder.getPosition();
+        return encoder.getPosition() + ShooterHoodConstants.SHOOTER_NO_RETRACTION_ANGLE;
     }
 }
